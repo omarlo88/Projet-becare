14d7bb1a6b197522c859676fc238ce52a3767534
omarlo88 <omarlo88.omar@gmail.com>
2019-04-02 12:43:33 +0200

    first commit -- add pagination

---
+++ src/app/authentication/liste-users/liste-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-users',
+  templateUrl: './liste-users.component.html',
+  styleUrls: ['./liste-users.component.css']
+})
+export class ListeUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
